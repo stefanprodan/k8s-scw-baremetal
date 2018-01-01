c7e62fa9a560381e04817d8419faf04d6473f225
Stefan Prodan <stefan.prodan@gmail.com>
2018-01-01 17:48:38 +0200

    move master IP to its own file

--- main.tf
+++ main.tf
@@ -7,6 +7,3 @@ data "scaleway_image" "xenial" {
   name         = "Ubuntu Xenial"
 }
 
-resource "scaleway_ip" "k8s_master_ip" {
-  count = 1
-}
--- master.tf
+++ master.tf
@@ -1,3 +1,7 @@
+resource "scaleway_ip" "k8s_master_ip" {
+  count = 1
+}
+
 resource "scaleway_server" "k8s_master" {
   count     = 1
   name      = "k8s-master-${count.index + 1}"
@@ -19,9 +23,7 @@ resource "scaleway_server" "k8s_master" {
     inline = [
       "chmod +x /tmp/kubeadm-install.sh && /tmp/kubeadm-install.sh",
       "kubeadm init --apiserver-advertise-address=${self.private_ip} --kubernetes-version ${var.k8s_version}",
-      "mkdir -p $HOME/.kube",
-      "cp -i /etc/kubernetes/admin.conf $HOME/.kube/config",
-      "chown $(id -u):$(id -g) $HOME/.kube/config",
+      "mkdir -p $HOME/.kube && cp -i /etc/kubernetes/admin.conf $HOME/.kube/config",
       "kubectl apply -f \"https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')\"",
     ]
   }
