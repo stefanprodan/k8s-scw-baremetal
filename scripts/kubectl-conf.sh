f0619d67015d19c3ff491dc65aa607fbba144026
Aurélien Perrier <a.perrier89@gmail.com>
2018-01-26 22:29:32 +0100

    Fixing kubeconfig filename & terraform output

--- scripts/kubectl-conf.sh
+++ scripts/kubectl-conf.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-ARCH=$1
+WORKSPACE=$1
 PUBLIC_IP=$2
 PRIVATE_IP=$3
 
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${PUBLIC_IP}:/etc/kubernetes/admin.conf .
-sed -e "s/${PRIVATE_IP}/${PUBLIC_IP}/g" admin.conf > ${ARCH}.conf
+sed -e "s/${PRIVATE_IP}/${PUBLIC_IP}/g" admin.conf > ${WORKSPACE}.conf
 rm admin.conf
