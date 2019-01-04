03563ea4af12cc7a634b17e847b3eb2f3fe884a2
Stephen Moloney <stephen@stephenmoloney.com>
2019-01-04 23:23:50 +0000

    Improve kubeadm install script
    ***What does this commit/MR/PR do?***
    
    - Runs repetitive code inside a loop
    - Adds cri-tools to the list of deps
    - Stdouts in yellow warning colour when a dep is not available in the
    same version as kubernetes itself.
    
    ***Why is this commit/MR/PR needed?***
    
    - Better visibility of with anomalous versions
    - Adds cri-tools

--- scripts/kubeadm-install.sh
+++ scripts/kubeadm-install.sh
@@ -7,7 +7,24 @@ set -e
 curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update -qq
-apt-get install -qy --allow-downgrades \
-  kubeadm=$(apt-cache madison kubeadm | grep $(echo ${K8_VERSION#"stable-"}) | head -1 | awk '{print $3}') \
-  kubelet=$(apt-cache madison kubelet | grep $(echo ${K8_VERSION#"stable-"}) | head -1 | awk '{print $3}') \
-  kubectl=$(apt-cache madison kubectl | grep $(echo ${K8_VERSION#"stable-"}) | head -1 | awk '{print $3}')
+
+declare -a deps
+export deps=(kubeadm kubelet kubectl cri-tools)
+
+for dep in "${deps[@]}"""; do
+
+  dep_version=$(echo "${dep}_version" | tr - _)
+  if [[ -z "$(apt-cache madison ${dep} | grep ${K8_VERSION#"stable-"})" ]]; then
+    export ${dep_version}="$(apt-cache madison "${dep}" | head -1 | awk '{print $3}')"
+    echo -e """
+\033[33mWarning: ${dep} version ${K8_VERSION#"stable-"}.x is not available, \
+installing ${dep} $(apt-cache madison "${dep}" | head -1 | awk '{print $3}') instead\033[0m
+    """ && \
+    sleep 2s
+  else
+    export ${dep_version}="$(apt-cache madison "${dep}" | grep "${K8_VERSION#"stable-"}" | head -1 | awk '{print $3}')"
+  fi
+
+  apt-get install -qy --allow-downgrades "${dep}"="${!dep_version}"
+done
+
