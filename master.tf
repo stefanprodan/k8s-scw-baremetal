692eef529e28b8caa23060f9a9cac092de685344
Stephen Moloney <stephen@stephenmoloney.com>
2019-01-06 11:38:24 +0000

    Fix issue with excessively eager initialDelaySeconds on arm devices
    ***What does this commit/MR/PR do?***
    
    - Fix issue with excessively eager initialDelaySeconds on arm devices
    
    ***Why is this commit/MR/PR needed?***
    
    - Because the kubelet is blocking the cluster being started properly

--- master.tf
+++ master.tf
@@ -30,14 +30,39 @@ resource "scaleway_server" "k8s_master" {
   }
   provisioner "remote-exec" {
     inline = [
-      "set -e",
-      "chmod +x /tmp/docker-install.sh && /tmp/docker-install.sh ${var.docker_version}",
-      "chmod +x /tmp/kubeadm-install.sh && /tmp/kubeadm-install.sh ${var.k8s_version}",
-      "kubeadm init --apiserver-advertise-address=${self.private_ip} --apiserver-cert-extra-sans=${self.public_ip} --kubernetes-version=${var.k8s_version} --ignore-preflight-errors=KubeletVersion",
-      "mkdir -p $HOME/.kube && cp -i /etc/kubernetes/admin.conf $HOME/.kube/config",
-      "kubectl create secret -n kube-system generic weave-passwd --from-literal=weave-passwd=${var.weave_passwd}",
-      "kubectl apply -f \"https://cloud.weave.works/k8s/net?password-secret=weave-passwd&k8s-version=$(kubectl version | base64 | tr -d '\n')\"",
-      "chmod +x /tmp/monitoring-install.sh && /tmp/monitoring-install.sh ${var.arch}",
+      <<EOT
+#!/bin/bash
+set -e
+chmod +x /tmp/docker-install.sh
+chmod +x /tmp/kubeadm-install.sh
+chmod g+w -R /tmp/kubeadm/
+
+/tmp/docker-install.sh ${var.docker_version} && \
+/tmp/kubeadm-install.sh ${var.k8s_version}
+
+modify_kube_apiserver_config(){
+  while [[ ! -e /etc/kubernetes/manifests/kube-apiserver.yaml ]]; do
+    sleep 0.5s;
+  done && \
+  sed -i 's/failureThreshold: [0-9]/failureThreshold: 12/g' /etc/kubernetes/manifests/kube-apiserver.yaml && \
+  sed -i 's/timeoutSeconds: [0-9][0-9]/timeoutSeconds: 20/g' /etc/kubernetes/manifests/kube-apiserver.yaml && \
+  sed -i 's/initialDelaySeconds: [0-9][0-9]/initialDelaySeconds: 120/g' /etc/kubernetes/manifests/kube-apiserver.yaml
+}
+
+# ref https://github.com/kubernetes/kubeadm/issues/413 (initialDelaySeconds is too eager)
+if [[ ${var.arch} == "arm" ]]; then modify_kube_apiserver_config & fi
+
+kubeadm init \
+  --apiserver-advertise-address=${self.private_ip} \
+  --apiserver-cert-extra-sans=${self.public_ip} \
+  --kubernetes-version=${var.k8s_version} \
+  --ignore-preflight-errors=KubeletVersion
+
+mkdir -p $HOME/.kube && cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && \
+kubectl create secret -n kube-system generic weave-passwd --from-literal=weave-passwd=${var.weave_passwd} && \
+kubectl apply -f "https://cloud.weave.works/k8s/net?password-secret=weave-passwd&k8s-version=$(kubectl version | base64 | tr -d '\n')" && \
+chmod +x /tmp/monitoring-install.sh && /tmp/monitoring-install.sh ${var.arch}
+EOT
     ]
   }
   provisioner "local-exec" {
